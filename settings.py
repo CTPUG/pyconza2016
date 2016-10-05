ca52ccb447d2422ebf84c80b0889105b7af6f8b9
Simon Cross <hodgestar@gmail.com>
2016-10-05 23:03:05 +0200

    Activate current events.

--- settings.py
+++ settings.py
@@ -62,9 +62,9 @@ WAFER_MENUS += (
           "url": reverse_lazy("wafer_full_schedule")},
          {"name": "schedule-next-up", "label": _("Next up"),
           # Fixed day until closer to the conference start
-          "url": "/schedule/current/?day=2016-10-05&time=08:30"},
+          # "url": "/schedule/current/?day=2016-10-05&time=08:30"},
           # change this once we're at time
-          # "url": reverse_lazy("wafer_current")},
+          "url": reverse_lazy("wafer_current")},
          {"name": "accepted-talks", "label": _("Accepted Talks"),
           "url": reverse_lazy("wafer_users_talks")},
          {"name": "speakers", "label": _("Speakers"),
